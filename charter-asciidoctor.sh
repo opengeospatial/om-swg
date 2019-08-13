4bdf01a95595914697e80fdef0956c823aac7b33
Ilkka Rinne <ilkka.rinne@spatineo.com>
2019-08-13 15:34:58 +0300

    Styling, asciidoctor charter script now also copies image files

--- charter-asciidoctor.sh
+++ charter-asciidoctor.sh
@@ -1,1 +1,3 @@
-docker run --rm -v $(pwd):/documents/ asciidoctor/docker-asciidoctor asciidoctor ./charter/om-swg-charter.adoc -o ./docs/charter/index.html
+docker run --rm -v $(pwd):/documents/ asciidoctor/docker-asciidoctor asciidoctor -a stylesheet=../styles/standard.css ./charter/om-swg-charter.adoc -o ./docs/charter/index.html
+mkdir docs/charter/images
+cp charter/images/*.* docs/charter/images/
