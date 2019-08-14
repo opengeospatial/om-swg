22ef00199b82341cc8e105f0416fe65ffb720fd7
Ilkka Rinne <ilkka.rinne@spatineo.com>
2019-08-14 09:34:55 +0300

    Removed mkdir of the docs/charter/images dir

--- charter-asciidoctor.sh
+++ charter-asciidoctor.sh
@@ -1,3 +1,2 @@
 docker run --rm -v $(pwd):/documents/ asciidoctor/docker-asciidoctor asciidoctor -a stylesheet=../styles/standard.css ./charter/om-swg-charter.adoc -o ./docs/charter/index.html
-mkdir docs/charter/images
 cp charter/images/*.* docs/charter/images/
